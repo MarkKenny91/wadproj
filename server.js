627994335794bb5ffc0d38acba90204ea43a85c2
teddansonswig <mark.038@gmail.com>
2015-12-08 19:47:35 +0000

    social media

--- server.js
+++ server.js
@@ -1,5 +1,34 @@
+//
+// # SimpleServer
+//
+// A simple chat server using Socket.IO, Express, and Async.
+//
 var http = require('http');
-http.createServer(function (req, res) {
-  res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Hello World\n');
-}).listen(8080);
+var path = require('path');
+var bodyParser = require('body-parser');
+var express = require('express');
+
+var router = express();
+var server = http.createServer(router);
+
+router.use(express.static(path.resolve(__dirname, 'client')));
+router.use(bodyParser.urlencoded({extended: true}));
+router.use(bodyParser.json());
+
+router.get('/', function(req, res){
+  res.render('index'); //Tells router to look in client for index.html
+});
+
+router.get('/get/json', function(req, res) {
+   res.setHeader('content-type', 'application/json');//setting header for http request as application/json - sends application & json back
+   
+});
+
+router.post('/post/json', function(req, res){
+  console.log(req.body);
+});
+
+server.listen(process.env.PORT || 3000, process.env.IP || "0.0.0.0", function(){
+  var addr = server.address();
+  console.log("Chat server listening at", addr.address + ":" + addr.port);
+});
